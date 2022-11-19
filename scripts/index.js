6b1f6e2206cfa3fb32138a61ddc1d35ed41996bb
austinp <auspendleton@gmail.com>
2022-11-18 17:33:46 -0800

    Stage 3

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,35 @@ const initialCards = [
     link: "../images/lago-di-braies.jpg",
   },
 ];
+
+const editButton = document.querySelector(".profile__edit-button");
+const closeButton = document.querySelector(".modal-button");
+
+let modal = document.querySelector(".modal");
+
+function toggleModal() {
+  modal.classList.toggle("modal__opened");
+}
+
+editButton.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+
+const profileName = document.querySelector(".profile__name");
+const profileTag = document.querySelector(".profile__tag");
+
+const formInputName = document.querySelector("#name");
+const formInputTag = document.querySelector("#about-me");
+
+formInputName.value = profileName.textContent;
+formInputTag.value = profileTag.textContent;
+
+const profileFormElement = document.querySelector(".modal__form");
+
+function handleProfileSubmit(e) {
+  e.preventDefault();
+
+  profileName.textContent = formInputName.value;
+  profileTag.textContent = formInputTag.value;
+
+  toggleModal();
+}
