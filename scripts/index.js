67b492b2ad90e773d45469b5861ceaa70ad749dc
austinp <auspendleton@gmail.com>
2022-11-22 20:54:24 -0800

    Final Submission 1.2

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,27 @@
 const initialCards = [
   {
     name: "Yosemite Valley",
-    link: "./images/yosemite-valley.jpg",
+    link: "images/yosemite-valley.jpg",
   },
   {
     name: "Lake Louise",
-    link: "./images/lake-louise.jpg",
+    link: "images/lake-louise.jpg",
   },
   {
     name: "Bald Mountains",
-    link: "./images/bald-mountains.jpg",
+    link: "images/bald-mountains.jpg",
   },
   {
     name: "Latemar",
-    link: "./images/latemar.jpg",
+    link: "images/latemar.jpg",
   },
   {
     name: "Vanoise National Park",
-    link: "./images/vanoise-national-park.jpg",
+    link: "images/vanoise-national-park.jpg",
   },
   {
     name: "Lago di Braies",
-    link: "./images/lago-di-braies.jpg",
+    link: "images/lago-di-braies.jpg",
   },
 ];
 
