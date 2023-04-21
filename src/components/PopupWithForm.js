02ca4cba6af6b3ad8cbf0a53a6fb0ce07e5672fb
austinp <auspendleton@gmail.com>
2023-04-21 11:27:25 -0600

    querySelector issue

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,19 +1,27 @@
 import Popup from "./Popup.js";
 
 class PopupWithForm extends Popup {
-  constructor({ popupSelector, handleFormSubmit }) {
+  constructor(popupSelector, handleFormSubmit, loadingButtonText) {
     super({ popupSelector });
+
+    this._formSelector = this._popupElement.querySelector(".modal__container");
 
     this._modalForm = this._popupElement.querySelector(".modal__container");
     this._modalFormInputs =
       this._popupElement.querySelectorAll(".modal__input");
     this._modalFormButton = this._popupElement.querySelector("modal__button");
     this._handleFormSubmit = handleFormSubmit;
+    this._submitButton = this._formSelector.querySelector(".modal__button");
+    this._loadingButtonText = loadingButtonText;
   }
   setEventListeners() {
     super.setEventListeners();
     this._popupElement.addEventListener("submit", this._handleSubmit);
   }
+  showLoading() {
+    this._submitButton.textContent = this._loadingButtonText;
+  }
+
   _getInputValues() {
     const inputValues = {};
 
